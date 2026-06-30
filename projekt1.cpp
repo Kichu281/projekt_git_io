b327e1feadf48727c8af8a4eef4200e5ac1e82ca
Michal Szarleja <michlak2811@gmail.com>
2026-06-30 18:55:19 +0200

    dodanie sprawdzanie liczby pierwszej

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,18 +2,31 @@
 
 using namespace std;
 
+bool czyPierwsza(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
     cin >> a >> b;
+
     int wyjscie;
+
     do {
         cout << endl;
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+
         cin >> wyjscie;
+
     } while(wyjscie != 0);
+
     return 0;
 }
